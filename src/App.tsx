e508306765d96a91f3f89919c705badd6c8438d9
FujihitoWatanabe <fuzihito26@hotmail.com>
2023-08-19 14:01:36 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Pracrice1 } from "./practices/Practis1";
+import { Pracrice2 } from "./practices/Practis2";
+import { Pracrice3 } from "./practices/Practis3";
+import { Pracrice4 } from "./practices/Practis4";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Pracrice1 />
+      <Pracrice2 />
+      <Pracrice3 />
+      <Pracrice4 />
+    </div>
+  );
+}
